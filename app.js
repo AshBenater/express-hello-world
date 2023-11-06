0cb131e51128215d9ec54ca71a0c03bb99bd2189
AshBenater <131183117+AshBenater@users.noreply.github.com>
2023-11-06 15:16:42 +0100

    Add a new route for the health endpoint

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html)); // Removed the extra colon here
+app.get("/", (req, res) => res.type('html').send(html)) // Removed the extra colon here
 app.get("/health", (req, res) => {
   res.sendStatus(200);
 });
